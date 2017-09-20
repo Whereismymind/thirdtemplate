ef61234347c51c00331669c96ce26fc229c321e0
Whereismymind <pavelcrazzy@gmail.com>
2017-09-20 09:32:33 +0300

    'me'

---
+++ js/more-btn.js
@@ -0,0 +1,44 @@
+$(document).ready(function(){
+		//first
+    $(".s-block-1 .btn-more").click(function(){
+        $('.s-block-1 .more').show(300);
+    });
+    $(".s-block-1 .close-btn").click(function(){
+        $('.s-block-1 .more').hide(300);
+    });
+    //second
+    $(".s-block-2 .btn-more").click(function(){
+        $('.s-block-2 .more').show(300);
+    });
+    $(".s-block-2 .close-btn").click(function(){
+        $('.s-block-2 .more').hide(300);
+    });
+    //third
+    $(".s-block-3 .btn-more").click(function(){
+        $('.s-block-3 .more').show(300);
+    });
+    $(".s-block-3 .close-btn").click(function(){
+        $('.s-block-3 .more').hide(300);
+    });
+    //fourth
+    $(".s-block-4 .btn-more").click(function(){
+        $('.s-block-4 .more').show(300);
+    });
+    $(".s-block-4 .close-btn").click(function(){
+        $('.s-block-4 .more').hide(300);
+    });
+    //fifth
+    $(".s-block-5 .btn-more").click(function(){
+        $('.s-block-5 .more').show(300);
+    });
+    $(".s-block-5 .close-btn").click(function(){
+        $('.s-block-5 .more').hide(300);
+    });
+    //sixth
+    $(".s-block-6 .btn-more").click(function(){
+        $('.s-block-6 .more').show(300);
+    });
+    $(".s-block-6 .close-btn").click(function(){
+        $('.s-block-6 .more').hide(300);
+    });
+});
